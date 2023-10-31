25009956c504f816869a8cf28b98efd379f34a7d
Luigi-Iorio <luigi0iorio@gmail.com>
2023-10-31 15:15:46 +0100

    feat: visualizzazione dati in console

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const pwd = document.getElementById("pwd");
 
 // richieste all'utente - assegnazione variabile
 const nome = prompt("scrivi il tuo nome");
+// visualizzazione in console
+console.log(nome);
+
 const cognome = prompt("scrivi il tuo cognome");
+// visualizzazione in console
+console.log(cognome);
+
 const colore = prompt("scrivi il tuo colore preferito");
+// visualizzazione in console
+console.log(colore);
 
 // inserimento dati in html
 pwd.innerHTML = `${nome}${cognome}${colore}21`;
+// visualizzazione in console
+console.log(pwd.innerHTML);
